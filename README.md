479e3df11f45814a023efb3bf02eaeef85033901
Nix <ravendarque@users.noreply.github.com>
2026-03-21 23:08:20 +0000

    Updated to latest version of Limn

--- README.md
+++
@@ -1,4 +0,0 @@
-My personal Limn. 
-
-Create your own at [https://ravendarque.github.io/ravendarque-limn/configurator/)]()
-or check out the repo at [https://github.com/ravendarque-limn]()
--- tiles/codeblock.js
+++ tiles/codeblock.js
@@ -5,9 +5,16 @@ export function render(tile) {
   const pre = document.createElement("pre");
   const code = document.createElement("code");
   code.textContent = tile.content;
-  if (tile.language) code.setAttribute("data-language", tile.language);
+  if (tile.language) {
+    code.className = `language-${tile.language}`;
+  }
   pre.appendChild(code);
   el.appendChild(pre);
+  queueMicrotask(() => {
+    if (typeof Prism !== "undefined" && tile.language) {
+      Prism.highlightElement(code);
+    }
+  });
   return el;
 }
 
